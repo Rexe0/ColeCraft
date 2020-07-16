e773abf386a83fa8cca54c516298ba4085598711
BuildTools <unconfigured@null.spigotmc.org>
2020-07-16 17:08:17 +1000

    Added several things related to blazing fortress. Including:

---
+++ src/com/Rexe0/Items/Materials/NetheriteScrap.java
@@ -0,0 +1,10 @@
+package com.Rexe0.Items.Materials;
+
+
+import org.bukkit.Material;
+
+public class Coal extends CustomMaterial {
+    public Coal() {
+        super(Material.COAL, 1, "Coal", "COMMON", "COAL", 2);
+    }
+}
